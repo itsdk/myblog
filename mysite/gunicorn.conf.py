076af9533c7e775c1431099d080c2aeca5eac017
lifestudy <gentcomeone@gmail.com>
2016-02-21 17:00:06 -0500

    Changed the HTML for the site

---
+++ mysite/gunicorn.conf.py
@@ -0,0 +1,8 @@
+bind = '127.0.0.1:8000'
+workers = 3
+user = "nobody"
+#command = '/opt/myblog/myenv/bin/gunicorn'
+#pythonpath = '/opt/myblog/myenv/mysite'
+#bind = '127.0.0.1:8001'
+#workers = 3
+#user = nobody
